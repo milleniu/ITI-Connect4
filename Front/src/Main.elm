1c52a30aaa8f3edd9c7c1d61b1b7f07dae64ed5c
Luc Perret <luc.perret@signature-code.com>
2020-05-19 17:26:50 +0200

    Update Main

--- Front/src/Main.elm
+++ Front/src/Main.elm
@@ -48,3 +48,44 @@ type alias Case =
   , id : Int
   }
 
+type CaseState
+  = Empty
+  | PlayerOne
+  | PlayerTwocaseString : CaseState -> String
+
+caseString caseState = 
+  case caseState of 
+    Empty -> 
+      "Empty"
+    PlayerOne -> 
+      "PlayerOne"
+    PlayerTwo -> 
+      "PlayerTwo"update : Msg -> Model -> Model
+
+update msg model =
+  case msg of
+    NewGame -> 
+      Debug.log("Click")
+      (createModel "0")
+    AddPawn id ->
+      Debug.log(String.fromInt(id))
+      (createModel "ATTENTION ADDPAWN PAS NEW GAME")createModel gameID =
+  { gameId = gameID,  board =[ [ Empty, Empty, Empty, Empty, Empty, Empty ],[ Empty, Empty, Empty, Empty, Empty, Empty ],[ Empty, Empty, Empty, Empty, Empty, Empty ],[ Empty, Empty, Empty, Empty, Empty, Empty ],[ Empty, Empty, Empty, Empty, Empty, Empty ],[ Empty, Empty, Empty, Empty, Empty, Empty ],[ Empty, Empty, Empty, Empty, Empty, Empty ]] }-- VIEW   view : Model -> Html Msg
+view model =
+  div [ ]
+    [ button
+        [ class "buttons", onClick NewGame ] [text "New Game" ]
+    , div 
+        [ class "gridContainer"
+        , style "display" "flex"
+        ]
+        (List.indexedMap (\i elm -> div 
+                            [ class "column", onClick (AddPawn i) ]
+                            (List.map(\n -> div
+                                              [ 
+                                                class (caseString n)
+                                              ]
+                                              [ 
+                                                li [] []
+                                              ]) elm)          ) model.board)
+    ]
