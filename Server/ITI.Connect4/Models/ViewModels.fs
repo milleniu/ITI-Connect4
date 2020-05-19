d46f8c159fb8a99db0140329de1f50bd04c0e522
loic <fabulet@intechinfo.fr>
2020-05-19 17:56:34 +0200

    Add ViewModels for getGame endpoint

---
+++ Server/ITI.Connect4/Models/ViewModels.fs
@@ -0,0 +1,7 @@
+﻿namespace ITI.Connect4.Models
+
+type BoardCellViewModel = string
+type BoardColumnViewModel = BoardCellViewModel array
+type BoardViewModel = BoardColumnViewModel array
+type TurnViewModel = string
+type BoardStateViewModel = { Board: BoardViewModel; Turn: TurnViewModel }
--- Server/ITI.Connect4/Services/Connect4Service.fs
+++ Server/ITI.Connect4/Services/Connect4Service.fs
@@ -6,7 +6,7 @@ open ITI.Connect4.Models
 
 type Connect4ServiceDependency = {
     NewGame : IMemoryCache -> Guid -> Result<Guid, string>
-    GetGame : IMemoryCache -> Guid -> Result<BoardState, string>
+    GetGame : IMemoryCache -> Guid -> Result<BoardStateViewModel, string>
 }
 
 module Connect4Service =
@@ -15,11 +15,30 @@ module Connect4Service =
         | true, _ -> Error ( sprintf "Game %s already exists" ( id.ToString() ) )
         | _ ->
             let newBoard : Board = Array.create 7 ( Array.create 6 None )
-            let newBoardState = { Board = newBoard; Turn = Red }
+            let newBoardState : BoardState = { Board = newBoard; Turn = Red }
             cache.Set( id, newBoardState, TimeSpan.FromDays (float 1) ) |> ignore
             Ok id
+            
+    let getGame (cache: IMemoryCache) (id: Guid) : Result<BoardStateViewModel, string> =
+        let prettyfyBoardState ({ Board = board; Turn = turn }: BoardState) : BoardStateViewModel =
+            let toViewModel (player: Player): TurnViewModel =
+                match player with
+                | Red -> "Red" 
+                | Yellow -> "Yellow"
 
-    let getGame (cache: IMemoryCache) (id: Guid) : Result<BoardState, string> =
+            let boardViewModel: BoardViewModel =
+                board
+                |> Seq.map (fun (column: BoardColumn) ->
+                    column
+                    |> Seq.map (fun (cell: BoardCell) -> 
+                        match cell with
+                        | None -> "Empty"
+                        | Some player -> player |> toViewModel)
+                    |> Seq.toArray)
+                |> Seq.toArray
+
+            { Board = boardViewModel; Turn = turn |> toViewModel }
+        
         match cache.TryGetValue id with
-        | true, boardState -> Ok (boardState :?> BoardState)
+        | true, boardState -> Ok ( (boardState :?> BoardState) |> prettyfyBoardState )
         | _ -> Error ( sprintf "Game %s does not exists" (id.ToString() ) )
