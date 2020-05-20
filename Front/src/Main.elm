b97a10181bd2ce6647c9341a44d64b8fccae7a3e
Pingouin <me@pingouin.io>
2020-05-20 17:19:51 +0200

    Update main.elm

--- Front/src/Main.elm
+++ Front/src/Main.elm
@@ -2,17 +2,26 @@ module Main exposing (..)
 
 import Browser
 import List
-import Html exposing (..)
 import List.Extra exposing (..)
+import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, string)
 
 -- INTERNAL MODULES
+
+-- todo: http request
 
 -- import Styles exposing (..)
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
 
 -- MODEL
@@ -20,34 +29,91 @@ main =
 type alias Model = 
   {
     gameId : String,
-    board : List (List CaseState)
+    board : List (List State),
+    turn : State,
+    error : String,
+    gameState : String
   }
 
-init : Model
-init = 
-  Model
-  "0"
-  [
-    [ Empty, Empty, Empty, Empty, Empty, Empty ],
-    [ PlayerOne, Empty, Empty, Empty, Empty, Empty ],
-    [ PlayerTwo, Empty, Empty, Empty, Empty, Empty ],
-    [ Empty, Empty, Empty, Empty, Empty, Empty ],
-    [ Empty, Empty, Empty, Empty, Empty, Empty ],
-    [ Empty, Empty, Empty, Empty, Empty, Empty ],
-    [ Empty, Empty, Empty, Empty, Empty, Empty ]
-  ]
+init : () -> (Model, Cmd Msg)
+init _ = 
+  ( (createModel "-1"), getGame )
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 -- UPDATE
 
 type Msg 
   = AddPawn Int
-  | NewGame 
+  | NewGame
+  | GetGame (Result Http.Error String)
+  | PutPlay (Result Http.Error String)
+  | GetGrid (Result Http.Error (List (List State)))
 
 type alias Case =
-  { state : CaseState
+  { state : State
   , id : Int
   }
 
+type State
+  = Empty
+  | Yellow
+  | Red
+
+caseString : State -> String
+caseString state = 
+  case state of 
+    Empty -> 
+      "Empty"
+    Yellow -> 
+      "Yellow"
+    Red -> 
+      "Red"
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NewGame -> 
+      Debug.log "Click"
+      (createModel "", getGame)
+    AddPawn column ->
+      (model, putPlay model column)
+    
+    GetGame result -> 
+      case result of
+        Ok id -> 
+          Debug.log id
+          ( createModel id, Cmd.none)
+        Err _ -> ( addErrorModel model "Game error, restart a new game. (GetGame)" , Cmd.none)
+    PutPlay result -> 
+      case result of
+        Ok gameState -> 
+          ( updateGameState model gameState, getGrid model)
+        Err _ ->
+          ( addErrorModel model "Game error, restart a new game. (PutPlay)", Cmd.none)
+    GetGrid result -> 
+      case result of
+        Ok newGrid -> 
+          ( updateGrid model newGrid |> updateTurn, Cmd.none) 
+        Err _ -> ( addErrorModel model "Game error, restart a new game. (GetGrid)" , Cmd.none)
+
+ -- HELPER
+
+updateGrid : Model -> List (List State) -> Model
+updateGrid model grid =
+  { gameId = model.gameId, board = grid, turn = model.turn, error = model.error, gameState = model.gameState}
+
+updateGameState : Model -> String -> Model
+updateGameState model gameState = 
+  { gameId = model.gameId,  board = model.board, turn = model.turn, error = model.error, gameState = gameState}
+
+-- todo
 type CaseState
   = Empty
   | PlayerOne
