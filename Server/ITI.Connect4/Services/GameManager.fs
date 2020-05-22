80a6034777ba9f33f5b27e30a2f2332986d1c4b5
Hugo Zammit <hugo.zammit@signature-code.com>
2020-05-22 02:20:35 +0200

    Finalize evaluateBoard implementation.

--- Server/ITI.Connect4/Services/GameManager.fs
+++ Server/ITI.Connect4/Services/GameManager.fs
@@ -8,6 +8,8 @@ type GameManagerDependency = {
     EvaluateBoad : Board -> GameState
 }
 
+type WinningCondition = WinningCondition of (Column -> Row -> Player option)
+
 module GameManager =
     [<Literal>]
     let BoardWidth = 7
@@ -30,11 +32,11 @@ module GameManager =
             board.[column] |> Array.tryFindIndex Option.isNone
 
         let insertAt column row =
-            let chipAt c r =
-                if (c, r) = (column, row)
+            let chipAt column' row' =
+                if (column', row') = (column, row)
                     then Some player
-                    else board.[c].[r]
-            Array.init BoardWidth (fun c -> Array.init BoardHeight (fun r -> (chipAt c r)))
+                    else board.[column'].[row']
+            Array.init BoardWidth (fun column' -> Array.init BoardHeight (fun row' -> (chipAt column' row')))
 
         column
         |> validateColumn 
@@ -44,19 +46,44 @@ module GameManager =
             | None -> Error (sprintf "No space left in column %d" column))
 
     let evaluateBoard (board: Board) : GameState =
-        let winningConditions = [ ]
-            //match (i, j)
-            //if board.[j].[i] = board.[j].[i+1] && board.[j].[i] = board.[j].[i+2] && board.[j].[i] = board.[j].[i+3]
-            //    then match board.[j].[i] with Some p -> Win p | None -> Running
-            //    else Running))
+        let inline (>=<) a (b,c) = a >= b && a <= c
 
-        let checkAt col row : Player option =
-            None
+        let horizontalCheck = WinningCondition(fun column row ->
+            if (column >=< (0, BoardWidth - 4)) && (row >=< (0, BoardHeight - 1))
+            && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column+i].[row]))
+                then board.[column].[row]
+                else None)
+
+        let verticalCheck = WinningCondition(fun column row ->
+            if (column >=< (0, BoardWidth - 1)) && (row >=< (0, BoardHeight - 4))
+            && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column].[row + i]))
+                then board.[column].[row]
+                else None)
+
+        let ascendingDiagonal = WinningCondition(fun column row ->
+            if (column >=< (3, BoardWidth - 1)) && (row >=< (0, BoardHeight - 4))
+            && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column - i].[row + i]))
+                then board.[column].[row]
+                else None)
+
+        let descendingDiagonal = WinningCondition(fun column row ->
+            if (column >=< (3, BoardWidth - 1)) && (row >=< (3, BoardHeight - 1))
+            && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column - i].[row - i]))
+                then board.[column].[row]
+                else None)
+
+        let checkAt column row : Player option =
+            let rec checkAt' checks =
+                match checks with
+                | [] -> None
+                | WinningCondition( head ) :: tail -> match head column row with Some _ as winner -> winner | None -> checkAt' tail
+            checkAt' [ horizontalCheck ; verticalCheck ; ascendingDiagonal ; descendingDiagonal ]
 
         seq {
-            for col in 0..(BoardHeight - 1) do
-                for row in 0..(BoardWidth - 1) do
-                    if (checkAt col row |> Option.isSome) then Running (* Win of Player *) }
+            for col in 0..BoardHeight do
+                for row in 0..BoardWidth do
+                    let checkResult = checkAt col row
+                    if checkResult |> Option.isSome then Win checkResult.Value }
         |> Seq.fold (fun a b ->
             match (a, b) with
             | (Win _ as win), _ -> win
