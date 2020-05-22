230345b2c5f3e6f7ea9cb47cc3bf0ea8ef3c2a35
Hugo Zammit <hugo.zammit@signature-code.com>
2020-05-22 18:59:16 +0200

    Add Draw detection.

--- Server/ITI.Connect4/Services/GameManager.fs
+++ Server/ITI.Connect4/Services/GameManager.fs
@@ -35,39 +35,39 @@ type GameManager () =
                 | None -> Error (sprintf "No space left in column %d" column))
 
         member __.EvaluateBoard (board: Board) : GameState =
-            let inline (>=<) a (b,c) = a >= b && a <= c
-
-            let horizontalCheck = GameWinningCondition(fun column row ->
-                if (column >=< (0, Constants.BoardWidth - 4)) && (row >=< (0, Constants.BoardHeight - 1))
-                && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column+i].[row]))
-                    then board.[column].[row]
-                    else None)
-
-            let verticalCheck = GameWinningCondition(fun column row ->
-                if (column >=< (0, Constants.BoardWidth - 1)) && (row >=< (0, Constants.BoardHeight - 4))
-                && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column].[row + i]))
-                    then board.[column].[row]
-                    else None)
-
-            let ascendingDiagonal = GameWinningCondition(fun column row ->
-                if (column >=< (3, Constants.BoardWidth - 1)) && (row >=< (0, Constants.BoardHeight - 4))
-                && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column - i].[row + i]))
-                    then board.[column].[row]
-                    else None)
-
-            let descendingDiagonal = GameWinningCondition(fun column row ->
-                if (column >=< (3, Constants.BoardWidth - 1)) && (row >=< (3, Constants.BoardHeight - 1))
-                && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column - i].[row - i]))
-                    then board.[column].[row]
-                    else None)
-
             let checkAt column row : Player option =
+                let inline (>=<) a (b,c) = a >= b && a <= c
+                
+                let horizontalCheck = GameWinningCondition(fun column row ->
+                    if (column >=< (0, Constants.BoardWidth - 4)) && (row >=< (0, Constants.BoardHeight - 1))
+                    && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column+i].[row]))
+                        then board.[column].[row]
+                        else None)
+                
+                let verticalCheck = GameWinningCondition(fun column row ->
+                    if (column >=< (0, Constants.BoardWidth - 1)) && (row >=< (0, Constants.BoardHeight - 4))
+                    && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column].[row + i]))
+                        then board.[column].[row]
+                        else None)
+                
+                let ascendingDiagonalCheck = GameWinningCondition(fun column row ->
+                    if (column >=< (3, Constants.BoardWidth - 1)) && (row >=< (0, Constants.BoardHeight - 4))
+                    && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column - i].[row + i]))
+                        then board.[column].[row]
+                        else None)
+                
+                let descendingDiagonalCheck = GameWinningCondition(fun column row ->
+                    if (column >=< (3, Constants.BoardWidth - 1)) && (row >=< (3, Constants.BoardHeight - 1))
+                    && ([1..3] |> Seq.forall (fun i -> board.[column].[row] = board.[column - i].[row - i]))
+                        then board.[column].[row]
+                        else None)
+
                 let rec checkAt' checks =
                     match checks with
                     | [] -> None
                     | GameWinningCondition( head ) :: tail ->
                         match head column row with Some _ as winner -> winner | None -> checkAt' tail
-                checkAt' [ horizontalCheck ; verticalCheck ; ascendingDiagonal ; descendingDiagonal ]
+                checkAt' [ horizontalCheck ; verticalCheck ; ascendingDiagonalCheck ; descendingDiagonalCheck ]
 
             seq {
                 for col in 0..Constants.BoardHeight do
@@ -78,6 +78,5 @@ type GameManager () =
                 match (a, b) with
                 | (Win _ as win), _ -> win
                 | _, (Win _ as win) -> win
-                | _ -> Running ) Running
-            // TODO: Add Draw Check
+                | _ -> if (board |> Seq.forall (fun col -> col |> Seq.forall Option.isSome)) then Draw else Running) Running
             
